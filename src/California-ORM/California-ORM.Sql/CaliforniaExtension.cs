9770c01e9b60fdd1ea8d350348af2c921da990c2
Burim Hajrizaj <burimhajrizaj@gmail.com>
2023-06-01 18:48:06 -0700

    Update CaliforniaExtension.cs

--- src/California-ORM/California-ORM.Sql/CaliforniaExtension.cs
+++ src/California-ORM/California-ORM.Sql/CaliforniaExtension.cs
@@ -9,171 +9,171 @@ namespace California_ORM.Sql;
 
 public static class CaliforniaExtension
 {
-    private record TableSchemaName(string TableName, string Schema);
+    //private record TableSchemaName(string TableName, string Schema);
 
-    public static int Insert<T>(this SqlConnection connection, T entity, SqlTransaction? transaction = null)
-    {
-        var sql = "INSERT INTO [{0}].[{1}] ([{2}]) VALUES ('{3}')";
-        var tableName = GetEntityName(typeof(T));
-        var properties = GetPropertiesWithValues(entity, x => !x.GetCustomAttributes<IgnoreMember>().Any());
+    //public static int Insert<T>(this SqlConnection connection, T entity, SqlTransaction? transaction = null)
+    //{
+    //    var sql = "INSERT INTO [{0}].[{1}] ([{2}]) VALUES ('{3}')";
+    //    var tableName = GetEntityName(typeof(T));
+    //    var properties = GetPropertiesWithValues(entity, x => !x.GetCustomAttributes<IgnoreMember>().Any());
             
-        var propertyNames = properties.Select(x => x.Key);
-        var propertyValues = properties.Select(x => x.Value);
+    //    var propertyNames = properties.Select(x => x.Key);
+    //    var propertyValues = properties.Select(x => x.Value);
 
-        var propertyNameJoin = string.Join("], [", propertyNames);
-        var propertyValueJoin = string.Join("', '", propertyValues);
+    //    var propertyNameJoin = string.Join("], [", propertyNames);
+    //    var propertyValueJoin = string.Join("', '", propertyValues);
             
-        var insertSql = string.Format(sql, tableName.Schema, tableName.TableName, propertyNameJoin, propertyValueJoin);
+    //    var insertSql = string.Format(sql, tableName.Schema, tableName.TableName, propertyNameJoin, propertyValueJoin);
 
-        var cmd = connection.CreateCommand();
-        cmd.CommandText = insertSql;
-        cmd.Transaction = transaction;
-        return cmd.ExecuteNonQuery();
-    }
+    //    var cmd = connection.CreateCommand();
+    //    cmd.CommandText = insertSql;
+    //    cmd.Transaction = transaction;
+    //    return cmd.ExecuteNonQuery();
+    //}
 
-    public static int Delete<T>(this SqlConnection connection, T entity, SqlTransaction? transaction = null)
-    {
-        var sql = "DELETE FROM [{0}].[{1}] WHERE [{2}] = '{3}'";
-        var tableName = GetEntityName(typeof(T));
-        var primaryKeyFields = GetPropertiesWithValues(entity, x => x.GetCustomAttributes<PrimaryKey>().Any()).Single();
+    //public static int Delete<T>(this SqlConnection connection, T entity, SqlTransaction? transaction = null)
+    //{
+    //    var sql = "DELETE FROM [{0}].[{1}] WHERE [{2}] = '{3}'";
+    //    var tableName = GetEntityName(typeof(T));
+    //    var primaryKeyFields = GetPropertiesWithValues(entity, x => x.GetCustomAttributes<PrimaryKey>().Any()).Single();
         
-        var deleteSql = string.Format(sql, tableName.Schema, tableName.TableName, primaryKeyFields.Key, primaryKeyFields.Value);
+    //    var deleteSql = string.Format(sql, tableName.Schema, tableName.TableName, primaryKeyFields.Key, primaryKeyFields.Value);
 
-        var cmd = connection.CreateCommand();
-        cmd.CommandText = deleteSql;
-        cmd.Transaction = transaction;
-        return cmd.ExecuteNonQuery();
-    }
+    //    var cmd = connection.CreateCommand();
+    //    cmd.CommandText = deleteSql;
+    //    cmd.Transaction = transaction;
+    //    return cmd.ExecuteNonQuery();
+    //}
 
 
-    public static int Update<T>(this SqlConnection connection, T entity, SqlTransaction transaction = null)
-    {
-        var sql = @"UPDATE [{0}].[{1}]
-                   SET 
-                    {2}
-                     WHERE [{3}] = '{4}'";
+    //public static int Update<T>(this SqlConnection connection, T entity, SqlTransaction transaction = null)
+    //{
+    //    var sql = @"UPDATE [{0}].[{1}]
+    //               SET 
+    //                {2}
+    //                 WHERE [{3}] = '{4}'";
 
-        var tableName = GetEntityName(typeof(T));
-        var fields = GetPropertiesWithValues(entity, x => !x.GetCustomAttributes<IgnoreMember>().Any() && !x.GetCustomAttributes<PrimaryKey>().Any());
-        var primaryKeyFields = GetPropertiesWithValues(entity, x => x.GetCustomAttributes<PrimaryKey>().Any()).Single();
-        var updateFields = new List<string>();
-        foreach (var f in fields)
-        {
-            var str= "[" + f.Key + "] = '" + f.Value + "'";
-            updateFields.Add(str);
-        }
+    //    var tableName = GetEntityName(typeof(T));
+    //    var fields = GetPropertiesWithValues(entity, x => !x.GetCustomAttributes<IgnoreMember>().Any() && !x.GetCustomAttributes<PrimaryKey>().Any());
+    //    var primaryKeyFields = GetPropertiesWithValues(entity, x => x.GetCustomAttributes<PrimaryKey>().Any()).Single();
+    //    var updateFields = new List<string>();
+    //    foreach (var f in fields)
+    //    {
+    //        var str= "[" + f.Key + "] = '" + f.Value + "'";
+    //        updateFields.Add(str);
+    //    }
 
-        var updateFiledAndValue = string.Join(", \n", updateFields);
+    //    var updateFiledAndValue = string.Join(", \n", updateFields);
 
-        var updateSql = string.Format(sql, tableName.Schema, tableName.TableName, updateFiledAndValue, primaryKeyFields.Key, primaryKeyFields.Value);
+    //    var updateSql = string.Format(sql, tableName.Schema, tableName.TableName, updateFiledAndValue, primaryKeyFields.Key, primaryKeyFields.Value);
 
-        var cmd = connection.CreateCommand();
-        cmd.CommandText = updateSql;
-        cmd.Transaction = transaction;
-        return cmd.ExecuteNonQuery();
-    }
+    //    var cmd = connection.CreateCommand();
+    //    cmd.CommandText = updateSql;
+    //    cmd.Transaction = transaction;
+    //    return cmd.ExecuteNonQuery();
+    //}
 
-    public static T? Get<T>(this SqlConnection connection, object entityId, SqlTransaction? transaction = null) where T : class
-    {
-        var sql = "SELECT [{0}] FROM [{1}].[{2}] WHERE [{3}] = '{4}'";
-        var tableName = GetEntityName(typeof(T));
-        var primaryKey = typeof(T).GetProperties().Single(x => x.GetCustomAttributes<PrimaryKey>().Any());
+    //public static T? Get<T>(this SqlConnection connection, object entityId, SqlTransaction? transaction = null) where T : class
+    //{
+    //    var sql = "SELECT [{0}] FROM [{1}].[{2}] WHERE [{3}] = '{4}'";
+    //    var tableName = GetEntityName(typeof(T));
+    //    var primaryKey = typeof(T).GetProperties().Single(x => x.GetCustomAttributes<PrimaryKey>().Any());
 
-        var fields = GetProperties<T>(x => !x.GetCustomAttributes<IgnoreMember>().Any());
-        var fieldsJoin = string.Join("], [", fields);
+    //    var fields = GetProperties<T>(x => !x.GetCustomAttributes<IgnoreMember>().Any());
+    //    var fieldsJoin = string.Join("], [", fields);
 
-        var getSql = string.Format(sql,fieldsJoin, tableName.Schema, tableName.TableName, primaryKey.Name, entityId);
+    //    var getSql = string.Format(sql,fieldsJoin, tableName.Schema, tableName.TableName, primaryKey.Name, entityId);
 
-        var cmd = connection.CreateCommand();
-        cmd.CommandText = getSql;
-        cmd.Transaction = transaction;
-        var reader = cmd.ExecuteReader();
-        if (reader.Read())
-        {
-            var instance = Activator.CreateInstance<T>();
-            foreach (var field in fields)
-            {
-                var value = reader[field];
-                typeof(T).GetProperties().First(x => x.Name == field).SetValue(instance, value);
-            }
+    //    var cmd = connection.CreateCommand();
+    //    cmd.CommandText = getSql;
+    //    cmd.Transaction = transaction;
+    //    var reader = cmd.ExecuteReader();
+    //    if (reader.Read())
+    //    {
+    //        var instance = Activator.CreateInstance<T>();
+    //        foreach (var field in fields)
+    //        {
+    //            var value = reader[field];
+    //            typeof(T).GetProperties().First(x => x.Name == field).SetValue(instance, value);
+    //        }
 
-            return instance;
-        }
+    //        return instance;
+    //    }
 
-        return null;
-    }
+    //    return null;
+    //}
 
-    private static List<string> GetProperties<T>(Func<PropertyInfo, bool> expression)
-    {
-        var properties = new List<string>(20);
-        var propertyInfos = typeof(T).GetProperties().Where(expression);
+    //private static List<string> GetProperties<T>(Func<PropertyInfo, bool> expression)
+    //{
+    //    var properties = new List<string>(20);
+    //    var propertyInfos = typeof(T).GetProperties().Where(expression);
 
-        foreach (var propertyInfo in propertyInfos)
-        {
-            var propertyName = GetPropertyName(propertyInfo);
-            properties.Add(propertyName);
-        }
+    //    foreach (var propertyInfo in propertyInfos)
+    //    {
+    //        var propertyName = GetPropertyName(propertyInfo);
+    //        properties.Add(propertyName);
+    //    }
 
-        return properties;
-    }
+    //    return properties;
+    //}
 
-    private static Dictionary<string, object> GetPropertiesWithValues<T>(T entity, Func<PropertyInfo, bool> expression)
-    {
-        var properties = new Dictionary<string, object>();
-        var propertyInfos = typeof(T).GetProperties().Where(expression);
+    //private static Dictionary<string, object> GetPropertiesWithValues<T>(T entity, Func<PropertyInfo, bool> expression)
+    //{
+    //    var properties = new Dictionary<string, object>();
+    //    var propertyInfos = typeof(T).GetProperties().Where(expression);
 
-        foreach (var propertyInfo in propertyInfos)
-        {
-            var propertyName = GetPropertyName(propertyInfo);
-            var propertyValue = GetPropertyValue(propertyInfo, entity);
-            properties.Add(propertyName, propertyValue);
-        }
+    //    foreach (var propertyInfo in propertyInfos)
+    //    {
+    //        var propertyName = GetPropertyName(propertyInfo);
+    //        var propertyValue = GetPropertyValue(propertyInfo, entity);
+    //        properties.Add(propertyName, propertyValue);
+    //    }
 
-        return properties;
-    }
+    //    return properties;
+    //}
 
-    private static TableSchemaName GetEntityName(Type entityType)
-    {
-        var attributes = entityType.GetCustomAttributes<Table>();
-        if (attributes.Any())
-        {
-            var table = attributes.First();
-            return new TableSchemaName(table.Name, table.Schema);
-        }
+    //private static TableSchemaName GetEntityName(Type entityType)
+    //{
+    //    var attributes = entityType.GetCustomAttributes<Table>();
+    //    if (attributes.Any())
+    //    {
+    //        var table = attributes.First();
+    //        return new TableSchemaName(table.Name, table.Schema);
+    //    }
         
-        return new TableSchemaName(entityType.Name, "dbo");
-    }
+    //    return new TableSchemaName(entityType.Name, "dbo");
+    //}
 
-    private static string GetPropertyName(MemberInfo propertyInfo)
-    {
-        return propertyInfo.Name;
-    }
+    //private static string GetPropertyName(MemberInfo propertyInfo)
+    //{
+    //    return propertyInfo.Name;
+    //}
 
-    private static object? GetPropertyValue(PropertyInfo propertyInfo, object? entity)
-    {
-        if (entity == null)
-            return null;
+    //private static object? GetPropertyValue(PropertyInfo propertyInfo, object? entity)
+    //{
+    //    if (entity == null)
+    //        return null;
 
-        if (propertyInfo.PropertyType == typeof(DateTime))
-        {
-            var value = (DateTime)propertyInfo.GetValue(entity);
-            return value.ToString("yyyy-MM-dd HH:mm:ss.fff");
-        }
+    //    if (propertyInfo.PropertyType == typeof(DateTime))
+    //    {
+    //        var value = (DateTime)propertyInfo.GetValue(entity);
+    //        return value.ToString("yyyy-MM-dd HH:mm:ss.fff");
+    //    }
 
-        if (propertyInfo.PropertyType == typeof(DateOnly))
-        {
-            var value = (DateOnly)propertyInfo.GetValue(entity);
-            return value.ToString("yyyy-MM-dd");
-        }
+    //    if (propertyInfo.PropertyType == typeof(DateOnly))
+    //    {
+    //        var value = (DateOnly)propertyInfo.GetValue(entity);
+    //        return value.ToString("yyyy-MM-dd");
+    //    }
 
-        if (propertyInfo.PropertyType == typeof(string))
-        {
-            var value = (string)propertyInfo.GetValue(entity);
-            return value.Replace("'", "''");
-        }
+    //    if (propertyInfo.PropertyType == typeof(string))
+    //    {
+    //        var value = (string)propertyInfo.GetValue(entity);
+    //        return value.Replace("'", "''");
+    //    }
 
 
-        return propertyInfo.GetValue(entity);
-    }
+    //    return propertyInfo.GetValue(entity);
+    //}
 
 }
