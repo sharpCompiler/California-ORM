63c7cc55469270cd09df2ec57e34b1d117fbb8ee
Burim Hajrizaj <burimhajrizaj@gmail.com>
2023-06-01 19:34:33 -0700

    new start

--- src/California-ORM/California-ORM.Sql/PrimaryKey.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace California_ORM.Sql;
-
-[AttributeUsage(AttributeTargets.Property)]
-public class PrimaryKey: Attribute
-{
-}
---
+++ src/California-ORM/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace California_ORM
+{
+    public class Class1
+    {
+
+    }
+}
